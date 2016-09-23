c333f7b6e8c7c86b799621d900a16975fb9bf3b6
ccbayer <ccbayer@gmail.com>
2016-09-23 16:33:33 -0700

    CB add files

---
+++ entry.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+
+  render : function() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+});
+
+
+ReactDOM.render(<App/>, document.getElementById('body'));
