3c789a0dc7151ad310923affa0d2c49d1ad90afd
ccbayer <ccbayer@gmail.com>
2016-09-25 12:36:14 -0700

    CB additional setup 2

--- entry.js
+++ entry.js
@@ -1,12 +1,40 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var App = React.createClass({
 
+// for now load some sample data:
+var albums = require('./data/albums.js');
+
+var App = React.createClass({
+  getInitialState : function() {
+      return {
+        votes : {},
+        albums : {}
+      }
+  },
+  componentDidMount: function () {
+    // set initial state:
+    if(albums) {
+      this.setState({ albums : albums });
+    }
+  },
   render : function() {
     return (
       <div>
-        
+        <Albums/>
+      </div>
+    )
+  }
+});
+
+var Albums = React.createClass({
+  getRandomAlbums: function() {
+    console.log(this.state.albums);
+  },
+  render : function() {
+    return (
+      <div className="albums">
+        {this.getRandomAlbums}
       </div>
     )
   }
